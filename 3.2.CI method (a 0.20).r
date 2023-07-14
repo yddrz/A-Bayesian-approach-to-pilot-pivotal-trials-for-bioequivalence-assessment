45954a99ac8f21ce5349b84d9c624586f25c524f
Duo Lv <117263974+yddrz@users.noreply.github.com>
2023-07-15 07:43:19 +0800

    Add files via upload

--- 3.2.CI method (a 0.20).r
+++ 3.2.CI method (a 0.20).r
@@ -1,7 +1,7 @@
 library(nlme) #install the package nlme and PowerTOST first
 library(PowerTOST)#Power and Sample Size Based on Two One-Sided t-Tests (TOST) for (Bio)Equivalence Studies
 
-cimethod1<-function(pkdata,N) {
+cimethod2<-function(pkdata,N) {
   Y<-rep(99,10000)
   for (i in 1:N){
     
@@ -20,7 +20,7 @@ cimethod1<-function(pkdata,N) {
     meanRAUCLST<-res.by[2]
     PEAUCLST<- exp(meanTAUCLST-meanRAUCLST)    #the mean of uT-UR
     PEAUCLST  
-    CIAUCLST<- CI.BE(alpha=0.10,pe=PEAUCLST,CV=CVAUCLST, n=nrow(pkdata[[i]])/2, design = "2x2", robust=FALSE)
+    CIAUCLST<- CI.BE(alpha=0.20,pe=PEAUCLST,CV=CVAUCLST, n=nrow(pkdata[[i]])/2, design = "2x2", robust=FALSE)
     CIAUCLST  #upper limit of confidence interval  lower limit of confidence interval
     if (CIAUCLST[1]>=0.8 && CIAUCLST[2]<=1.25)
       Y[[i]]=1 else 
@@ -29,64 +29,55 @@ cimethod1<-function(pkdata,N) {
   Y
 }
 
-result1_1<-cimethod1(cmax1,10000)
-result1_2<-cimethod1(auct1,10000)
-
+result1_1<-cimethod2(cmax1,10000)
+result1_2<-cimethod2(auct1,10000)
 sum(result1_1)
 sum(result1_2)
 
 
-result2_1<-cimethod1(cmax2,10000)
-result2_2<-cimethod1(auct2,10000)
-
+result2_1<-cimethod2(cmax2,10000)
+result2_2<-cimethod2(auct2,10000)
 sum(result2_1)
 sum(result2_2)
 
 
-result3_1<-cimethod1(cmax3,10000)
-result3_2<-cimethod1(auct3,10000)
-
+result3_1<-cimethod2(cmax3,10000)
+result3_2<-cimethod2(auct3,10000)
 sum(result3_1)
 sum(result3_2)
 
 
-result4_1<-cimethod1(cmax4,10000)
-result4_2<-cimethod1(auct4,10000)
-
+result4_1<-cimethod2(cmax4,10000)
+result4_2<-cimethod2(auct4,10000)
 sum(result4_1)
 sum(result4_2)
 
 
-result5_1<-cimethod1(cmax5,10000)
-result5_2<-cimethod1(auct5,10000)
-
+result5_1<-cimethod2(cmax5,10000)
+result5_2<-cimethod2(auct5,10000)
 sum(result5_1)
 sum(result5_2)
 
 
-result6_1<-cimethod1(cmax6,10000)
-result6_2<-cimethod1(auct6,10000)
-
+result6_1<-cimethod2(cmax6,10000)
+result6_2<-cimethod2(auct6,10000)
 sum(result6_1)
 sum(result6_2)
 
 
-result7_1<-cimethod1(cmax7,10000)
-result7_2<-cimethod1(auct7,10000)
-
+result7_1<-cimethod2(cmax7,10000)
+result7_2<-cimethod2(auct7,10000)
 sum(result7_1)
 sum(result7_2)
 
 
-result8_1<-cimethod1(cmax8,10000)
-result8_2<-cimethod1(auct8,10000)
-
+result8_1<-cimethod2(cmax8,10000)
+result8_2<-cimethod2(auct8,10000)
 sum(result8_1)
 sum(result8_2)
 
 
-result9_1<-cimethod1(cmax9,10000)
-result9_2<-cimethod1(auct9,10000)
-
+result9_1<-cimethod2(cmax9,10000)
+result9_2<-cimethod2(auct9,10000)
 sum(result9_1)
 sum(result9_2)
